0f6b783f5e09dbf93e7afc4ec161eec37bfbd2f7
Mr-X-GTA <110748953+Mr-X-GTA@users.noreply.github.com>
2024-12-14 02:48:17 +0100

    Update gtav-classes (#60)

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
-    GIT_TAG        dee76e935485c13e8a660d3640aca9345a18a6f1
+    GIT_TAG        d2741287676b7009577d5b32b784859223138faa
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
